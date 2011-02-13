3afa026243547d5ffb4a255ab4710c52c9f93681
marcelcorso <marcel.corso@gmail.com>
2011-02-13 03:44:43 +0100

    transform in a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fluffy::Application.config.secret_token = '04431b0a258617a1a2fdd30daa7cade66e781f9e0720fc3cbfeb998b7102a307de5eebd50c6f7a40aed13e7781c3b56044ed17a548c0846bc966cdc8c0f94372'
